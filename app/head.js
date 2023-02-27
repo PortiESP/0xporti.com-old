7566a92037fea162cd39cc71ae1c058807b70b69
Porti <you@example.com>
2023-02-27 14:31:30 +0100

    Google Analytics

--- app/head.js
+++ app/head.js
@@ -1,5 +1,16 @@
 export default function Head(){
     return (<>
         <title>0xPorti</title>
+
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-242066778-1"></script>
+        <script>{`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-242066778-1');
+        `}</script>
+
     </>)
 }
