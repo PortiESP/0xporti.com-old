a3a2792eacab825d3e4186a0b08ccf5679a4536d
Porti <you@example.com>
2023-03-02 23:41:50 +0100

    Migrating to new head version 1

--- app/blog/post/head.js
+++
@@ -1,9 +0,0 @@
-import PostHead from "./PostHeadMetadata"
-
-
-export default async function Head(){
-
-    return (<>
-        <PostHead />
-    </>)
-}
--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,15 @@
 import Footer from '@/components/footer/Footer'
+import GoogleAnalytics from '@/components/GoogleAnalytics/GoogleAnalytics'
 import './globals.scss'
+
+export const metadata = {
+  title: "0xPorti",
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-
+      <GoogleAnalytics />
       <body>
         {children}
         <Footer />
