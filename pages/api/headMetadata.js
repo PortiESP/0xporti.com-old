9bd2e0b85ea26ab8b8da468e2aed6a1158dd7443
Porti <you@example.com>
2023-03-06 23:23:27 +0100

    New post

---
+++ pages/api/headMetadata.js
@@ -0,0 +1,9 @@
+
+const meta = {
+    "theme-color": "#2b2b2b",
+    "apple-mobile-web-app-status-bar": "#333",
+    "twitter:card": "summary",
+    "twitter:creator": "@PortiiRM"
+}
+
+export default meta
