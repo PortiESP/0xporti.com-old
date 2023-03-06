908e956e66c353cc3ef131a073f4031329c1a5c9
Porti <you@example.com>
2023-03-07 00:04:16 +0100

    Page "windows_shortcuts" published

--- app/blog/post/(posts)/windows_shortcuts/page.js
+++ app/blog/post/(posts)/windows_shortcuts/page.js
@@ -1,19 +1,103 @@
-import Tags from "@/components/MarkupWidgets/Tags.js"
+import {Title, Section, Callout, Paragraph, Code, Quote, UList} from "@/components/MarkupWidgets/Tags.js"
 import metadataList from "@/api/blog/postsData"
+import BubbleScrollBar from "@/components/BubbleScrollBar/BubbleScrollBar"
 
 
 
 export const metadata = metadataList.page
 
+const QUOTE_COLOR = "#07f4"
+const KEY_COLOR = "#00a2ff"
+
+const sections = [
+	{
+		label: "Screenshoots",
+		id: "screenshoots"
+	},
+	{
+		label: "Clipboard",
+		id: "clipboard"
+	},
+	{
+		label: "Desktops",
+		id: "desktops"
+	},
+	{
+		label: "Windows Apps",
+		id: "apps"
+	},
+]
+
 
 export default function Page(){ return (<>
 
-	<Tags.Title >
-		10 Windows Shortcuts
-	</Tags.Title>
+	<Title>
+		Poductivity Windows Shortcuts
+	</Title>
+
+	<Section title="Screenshoots" id="screenshoots" sticky>
+		<Callout label="info">This shortcut allow us to make screenshoots with diferent shapes</Callout>
+		<Paragraph>
+			This shortcut have 4 options for the shape of the screenshoot region
+		</Paragraph>
+		<UList items={[
+			<><b>Square</b> → Draw a square of the region we want to capture</>,
+			<><b>Free-form</b> → Draw a custom shape of the region we want to capture</>,
+			<><b>Window</b> → Select a window to capture (<i>chrome, excell, settings, …</i>)</>,
+			<><b>Fullscreen</b> → Capture the whole screen</>
+		]}/>
+		<Quote title="Shortcut" color={QUOTE_COLOR}>
+			<Code inline color={KEY_COLOR}>WIN</Code> + <Code inline color={KEY_COLOR}>Shift</Code> + <Code inline color={KEY_COLOR}>S</Code>
+		</Quote>
+	</Section>
 
 
+	<Section title="Clipboard" id="clipboard" sticky>
+		<Callout label="info">We can enable the clipboard history so we can copy many things and select what to paste from the history</Callout>
+		<Quote title="Shortcut" color={QUOTE_COLOR}>
+			<Code inline color={KEY_COLOR}>WIN</Code> + <Code inline color={KEY_COLOR}>V</Code>
+		</Quote>
+	</Section>
 
 
+	<Section title="Desktops" id="desktops" sticky>
+		<Callout label="info">Using multiple desktops allow us to organize our workspace and have a cleaner view of our opened apps</Callout>
+		<Paragraph>
+			We can open a new desktop to hide some program that we want to keep it active but dont want to see like music apps such as <i>spotify</i> or <i>itunes</i>.
+		</Paragraph>
+		<Quote title="Shortcut: Open desktops view" color={QUOTE_COLOR}>
+			<Code inline color={KEY_COLOR}>WIN</Code> + <Code inline color={KEY_COLOR}>TAB</Code>
+		</Quote>
+		<Quote title="Shortcut: Rotate between desktops" color={QUOTE_COLOR}>
+			<Code inline color={KEY_COLOR}>Ctrl</Code> + <Code inline color={KEY_COLOR}>WIN</Code> + <i>{"left/right arrows"}</i>
+		</Quote>
+	</Section>
+
+
+	<Section title="Windows apps" id="apps" sticky>
+		<Callout label="info">We have shortcuts for some windows apps such as the file explorer, magnifier, task manager, …</Callout>
+		<Quote title="Shortcut: Open pinned app (Task Bar)" color={QUOTE_COLOR}>
+			<Code inline color={KEY_COLOR}>WIN</Code> + <b>N</b>
+		</Quote>
+		<Quote title="Shortcut: Open File Explorer" color={QUOTE_COLOR}>
+			<Code inline color={KEY_COLOR}>WIN</Code> + <Code inline color={KEY_COLOR}>E</Code>
+		</Quote>
+		<Quote title="Shortcut: Task Manager" color={QUOTE_COLOR}>
+			<Code inline color={KEY_COLOR}>Ctrl</Code> + <Code inline color={KEY_COLOR}>WIN</Code> + <Code inline color={KEY_COLOR}>ESC</Code>
+		</Quote>
+		<Quote title="Shortcut: Settings" color={QUOTE_COLOR}>
+			<Code inline color={KEY_COLOR}>WIN</Code> + <Code inline color={KEY_COLOR}>I</Code>
+		</Quote>
+		<Quote title="Shortcut: Magnifier" color={QUOTE_COLOR}>
+			<Code inline color={KEY_COLOR}>WIN</Code> + <Code inline color={KEY_COLOR}>+</Code>
+			<UList items={[
+			<><Code inline color={KEY_COLOR}>WIN</Code> + <Code inline color={KEY_COLOR}>+</Code> → Zoom In</>,
+			<><Code inline color={KEY_COLOR}>WIN</Code> + <Code inline color={KEY_COLOR}>-</Code> → Zoom Oun</>
+		]}/>
+		</Quote>
+	</Section>
+
+
+	<BubbleScrollBar sections={sections} />
 
 </>)}
