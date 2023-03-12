84391a259c91e40bebb8d9f4c2b8dd2a429deed8
Porti <you@example.com>
2023-03-12 09:33:33 +0100

    Updated Meta Tags for social media

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,51 @@ import './globals.scss'
 
 export const metadata = {
   title: "0xPorti",
+  description: "Pesonal site of Porti, Full-Stack and Software Developer",
+  
+  // Color
+  themeColor: "#2b2b2b",
+  colorScheme: "dark",
+  category: "technology",
+  
+  // Credits
+  creator: "Porti",
+  authors: [{name:"Porti"}],
+
+  // Twitter
+  twitter: {
+    card: "summary",
+    creator: "@PortiiRM",
+    site: "https://www.0xPorti.com",
+    images: ["/assets/images/0xPorti_logo_icon.png"],
+    title: "0xPorti",
+  },
+  
+  // OpenGraph
+  OpenGraph: {
+    title: "0xPorti",
+    description: "Pentesting, Binary Exploitation, Software, ....",
+    type: "article",
+    image: "/assets/images/0xPorti_logo_icon.png",
+    alt: "0xPorti Icon",
+    height: "850",
+    width: "850",
+    locale: "en_ES"
+  },
+
+  // Icons
+  icons: {
+    icon: "/assets/images/0xPorti_logo_icon.png",
+    shortcut: "/assets/images/0xPorti_logo_icon.png",
+    apple: "/assets/images/0xPorti_logo_icon.png",
+  },
+
+  // Robots
+  robots: {
+    index: true,
+    follow: true
+  }
+
 }
 
 export default function RootLayout({ children }) {
--- pages/api/headMetadata.js
+++
@@ -1,9 +0,0 @@
-
-const meta = {
-    "theme-color": "#2b2b2b",
-    "apple-mobile-web-app-status-bar": "#333",
-    "twitter:card": "summary",
-    "twitter:creator": "@PortiiRM"
-}
-
-export default meta
