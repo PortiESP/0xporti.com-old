04f40d9cc7f2f85274a55b119c6fa99c98c5327c
porti <portiesp@gmail.com>
2023-05-19 00:49:28 +0200

    Testing component beta

---
+++ app/test/comp.jsx
@@ -0,0 +1,152 @@
+'use client'
+
+import sass from "./comp.module.scss"
+import sass2 from "./components.module.scss"
+import { useEffect, useState } from "react"
+
+export default function MindMap(props){
+
+    const [hide, setHide] = useState(false)
+    const [viewbox, setViewbox] = useState([0, 0, props.width, props.height])
+    const [mouseCoords, setMouseCoords] = useState([0, 0])
+    const [isHover, setIsHover] = useState(false)
+    const [isPressed, setIsPressed] = useState(false)
+    const [zoom, setZoom] = useState(1)
+    const baseInc = 0.9
+
+    const wheelEvent = e => {
+        const isMax = () => (viewbox[2]+baseInc > props.width) || (viewbox[3]+baseInc > props.height)
+
+        const inc = e.deltaY < 0 ? baseInc: 1/baseInc  // Handle zoom in/out
+        const diffW = viewbox[2] - viewbox[2]*inc  // Handle increment
+        const diffH = viewbox[3] - viewbox[3]*inc  // Handle increment
+        if (e.deltaY < 0 || !isMax() && !isPressed) setViewbox(old => [old[0]+diffW/2, old[1]+diffH/2, old[2]-diffW, old[3]-diffH])
+        setZoom(viewbox[2]/props.width)
+
+    }
+
+
+    const moveEvent = e => {
+        if (isPressed){
+            let deltaX;
+            let deltaY;
+
+            if (!mouseCoords[0] && !mouseCoords[1]) {
+                setMouseCoords([e.clientX, e.clientY])  // Initial state
+                deltaX = 0
+                deltaY = 0
+            }
+            else {
+                deltaX = (mouseCoords[0] - e.clientX) * (zoom*0.75)
+                deltaY = (mouseCoords[1] - e.clientY) * (zoom*0.75)
+            }
+
+            console.log(viewbox, deltaX, deltaY, zoom)  // DEBUG
+
+            setViewbox([viewbox[0]+deltaX, viewbox[1]+deltaY, viewbox[2], viewbox[3]])  // Move viewbox
+            setMouseCoords([e.clientX, e.clientY])  // Update coords
+        } 
+    }
+
+
+    useEffect(()=>{
+        document.body.style.overflow = (isHover ? "hidden" : "visible")
+    }, [isHover])
+
+
+
+    return (<div className={sass.div__wrap} 
+                onWheel={wheelEvent} 
+                onMouseEnter={()=>setIsHover(true)} 
+                onMouseLeave={()=>{setIsHover(false); setIsPressed(false)}}
+                onMouseDown={()=>{setIsPressed(true); setMouseCoords([0,0])}}
+                onMouseUp={()=>setIsPressed(false)}
+                onMouseMove={moveEvent}
+            >
+        {/* Image */}
+        <div className={sass.div__image}>
+            <svg viewBox={viewbox.join(" ")} fill="none" xmlns="http://www.w3.org/2000/svg">
+                {props.children}
+            </svg>
+        </div>
+        {/* Menu */}
+        <div className={sass.div__menu}>
+            {/* Menu header */}
+            <div className={sass.div__menu_header} onClick={()=>setHide(old => !old)}>
+                <span className={sass.span__header_title}>Menu</span>
+                <span className={sass.span__header_icon}></span>
+            </div>
+            {/* Menu items */}
+            <div className={[sass.div__menu_body, hide&&sass.state__hide].join(" ")}>
+                <hr/>
+                
+                <Toggle label="test" id="Ellipse1" initial={true}/>
+                <Toggle label="test" id="Ellipse2"/>
+                <Chamber label="Multiple" ids={["rect1", "rect2", "rect3", "rect4", "rect5", "rect6"]}/>
+            </div>
+        </div>
+        {/* Zoom label */}
+        <span className={sass.span__zoom} onClick={()=>{setZoom(1); setViewbox([0, 0, props.width, props.height])}}>
+            {Math.round((1/zoom)*100)}%
+        </span>
+    </div>)
+}
+
+
+
+// PROPS {label, id, initial}
+function Toggle(props){
+    
+    const [state, setState] = useState(props.initial || false)
+
+    useEffect(()=>{
+        console.log("Setting: ", props.id, " to ", state)
+        document.getElementById(props.id).style.visibility = state ? "visible" : "hidden"
+    }, [state])
+
+    return (<div className={[sass2.div__button_wrap, state ? sass2.state__on : sass2.state__off].join(" ")} title={props.label} onClick={()=>setState(old=>!old)}>
+        <div className={sass2.div__button_label}>{props.label}</div>
+        <div className={sass2.div__button_toggle}>{state ? "ON" : "OFF"}</div>
+    </div>)
+}
+
+// PROPS {label, ids, initial}
+function Chamber(props){
+    
+    const [selected, setSelected] = useState(props.ids[0])
+    const [state, setState] = useState(props.initial || false)
+    
+    // Selected listener
+    useEffect(()=>{
+        console.log("Setting selected to ", selected)
+
+        // Hide unselected
+        props.ids.map( id => document.getElementById(id).style.visibility = "hidden" ) 
+        // Show selected
+        document.getElementById(selected).style.visibility = "visible"
+        setState(true)
+
+    }, [selected])
+
+    // State listener
+    useEffect(()=>{
+        console.log("Setting: ", selected, " to ", state)
+        document.getElementById(selected).style.visibility = state ? "visible" : "hidden"
+    }, [state])
+
+    
+    // Initial settings
+    useEffect(()=> setState(false), [])
+
+
+
+    return (<div className={[sass2.div__chamber_wrap, state ? sass2.state__on : sass2.state__off].join(" ")} title={props.label} >
+        <div className={sass2.div__chamber_label}>{props.label}</div>
+        <div className={sass2.div__chamber_toggle} onClick={()=>setState(old=>!old)}>{state ? "ON" : "OFF"}</div>
+        <select className={sass2.select__chamber_selector} onChange={e=>setSelected(e.target.value)}>
+            {
+                props.ids.map( id => <option key={id} value={id}>{id}</option>)
+            }
+        </select>
+    </div>)
+}
--- app/test/page.js
+++ app/test/page.js
@@ -1,27 +1,25 @@
-import { Comment, Paragraph } from "@/components/MarkupWidgets/Tags";
-
+import MindMap from "./comp";
+import sass from "./page.module.scss"
 
 export default function Page(){
     return (<>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <Paragraph>
-        Lets comment something: <Comment comment="This is a comment, some more info, some more info to fill the line">hello</Comment> some more text
-
-        </Paragraph>
-
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
+        <div className={sass.div__wrap_frame}>
+            <div className={sass.div__frame}>
+                <MindMap width={892} height={562}>
+                    <rect width="892" height="562" fill="#F5F5F5"/>
+                    <g id="Frame 48">
+                    <rect width="892" height="562" fill="black"/>
+                    <circle id="Ellipse1" cx="253" cy="281" r="104" fill="#FF1C1C"/>
+                    <circle id="Ellipse2" cx="598" cy="281" r="104" fill="#0029FF"/>
+                    <rect id="rect1" x="108" y="457" width="59" height="59" rx="3" fill="#FF0000"/>
+                    <path id="rect3" d="M295 461C295 459.343 296.343 458 298 458H351C352.657 458 354 459.343 354 461V514C354 515.657 352.657 517 351 517H298C296.343 517 295 515.657 295 514V461Z" fill="#00FF0A"/>
+                    <path id="rect5" d="M494 461C494 459.343 495.343 458 497 458H550C551.657 458 553 459.343 553 461V514C553 515.657 551.657 517 550 517H497C495.343 517 494 515.657 494 514V461Z" fill="#AD00FF"/>
+                    <rect id="rect2" x="203" y="458" width="59" height="59" rx="3" fill="#FFF500"/>
+                    <rect id="rect4" x="390" y="458" width="59" height="59" rx="3" fill="#00F0FF"/>
+                    <rect id="rect6" x="589" y="458" width="59" height="59" rx="3" fill="#FF00E6"/>
+                    </g>
+                </MindMap>
+            </div>
+        </div>
     </>)
 }
